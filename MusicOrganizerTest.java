191591deb886cf325be36160361290792b01a848
Tone Berg <berton15@student.westerdals.no>
2015-12-13 15:09:52 +0100

    Exercise 4.1
    Also set up test fixture.

---
+++ MusicOrganizerTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class MusicOrganizerTest.
+ *
+ * @author  Tone Berg
+ * @version 2015.12.13
+ */
+public class MusicOrganizerTest
+{
+    private MusicOrganizer musicOrg1;
+
+    /**
+     * Default constructor for test class MusicOrganizerTest
+     */
+    public MusicOrganizerTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+        musicOrg1 = new MusicOrganizer();
+        musicOrg1.addFile("BigBillBroonzy-BabyPleaseDontGo1.mp3");
+        musicOrg1.addFile("BlindBlake-EarlyMorningBlues.mp3");
+        musicOrg1.addFile("BlindLemonJefferson-matchBoxBlues.mp3");
+        musicOrg1.addFile("BlindLemonJefferson-OneDimeBlues.mp3");
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
