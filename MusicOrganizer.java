171795c21fe4312abaaa37452697d76cdabc22a9
Tone Berg <berton15@student.westerdals.no>
2015-12-13 19:00:47 +0100

    Exercise 4.24

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -97,8 +97,10 @@ public class MusicOrganizer
      * List all files in the collection.
      */
     public void listAllFiles() {
+        int position = 0;
         for ( String filename : files ) {
-            System.out.println(filename);
+            System.out.println(position + ": " + filename);
+            position++;
         }
     }
 }
